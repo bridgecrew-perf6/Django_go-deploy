f17a3d175c5ccf4c37f9101a364352357f8a7f77
DeenaWilson <94810674+deenawilson@users.noreply.github.com>
2022-04-17 19:29:32 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,21 @@
+"""Django_go URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from django.http import HttpResponse
-from . import views 
 
 urlpatterns = [
-     path('', views.home, name='home'),    
-    path('contact/', views.contact, name='contact'),
-    path('forum/',views.forum, name='forum'),
-    path('about/', views.about, name='about'),
-    path('rule/', views.rule, name='rule'),
-   
-    path('register/', views.register_request, name="register"),
-    path("login/", views.loginPage, name="login")
-    path('logout/',views.logoutUser, name='logout'),
-    path("reset_pw/", auth_views.PasswordResetView.as_view(template_name='html/password_reset.html'),
-    name='reset_password' ),
-    path("reset_pw_send/", auth_views.PasswordResetDoneView.as_view(template_name='html/password_reset_sent.html'), 
-    name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='html/password_reset_form.html'), 
-    name='password_reset_confirm' ),
-    path("reset_pw_complete/", auth_views.PasswordResetCompleteView.as_view(template_name='html/password_reset_done.html'), 
-    name='password_reset_complete' ),
-
+    path('admin/', admin.site.urls),
 ]
