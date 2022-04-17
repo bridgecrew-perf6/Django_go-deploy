3ddaa7d43c60f4c10b628635bb4c9731fb4481f6
DeenaWilson <94810674+deenawilson@users.noreply.github.com>
2022-04-17 23:09:37 +0530

    Update models.py

--- Community/models.py
+++ Community/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+class customer(models.Model):
+    username=models.OneToOneField(User,null=True, on_delete=models.CASCADE, blank=True)
+    name=models.CharField(max_length=150, null=True)
+    contactno=models.CharField(max_length=150, null=True)
+    email=models.CharField(max_length=150, null=True)
+    
+    def __str__(self): #display name istead of id
+        return str(self.Name)
